1a6dfee3660a7e54e61036894aaa03a82c5fddd3
akumar <abhinav.kumar@navihealth.com>
2020-08-15 10:54:30 +0530

    Implemented template design pattern

--- src/main/java/com/abhi/behavioral/template/Main.java
+++ src/main/java/com/abhi/behavioral/template/Main.java
@@ -1,4 +1,15 @@
 package com.abhi.behavioral.template;
 
 public class Main {
+    public static void main(String[] args) {
+        HouseTemplate houseType = new WoodenHouse();
+
+        //using template method
+        houseType.buildHouse();
+        System.out.println("************");
+
+        houseType = new GlassHouse();
+
+        houseType.buildHouse();
+    }
 }
