2377d1211cbad84ba37cb82c1ede5412f0983025
akumar <abhinav.kumar@navihealth.com>
2020-08-18 08:21:54 +0530

    Added changes for iterator design pattern

---
+++ src/main/java/com/abhi/behavioral/iterator/ChannelIterator.java
@@ -0,0 +1,7 @@
+package com.abhi.behavioral.iterator;
+
+public interface ChannelIterator {
+    boolean hasNext();
+
+    Channel next();
+}
---
+++ src/main/java/com/abhi/behavioral/iterator/ChannelTypeEnum.java
@@ -0,0 +1,5 @@
+package com.abhi.behavioral.iterator;
+
+public enum ChannelTypeEnum {
+    ENGLISH, HINDI, FRENCH, ALL
+}
