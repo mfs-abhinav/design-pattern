f7f9a9466c1b5d045e40512bad680928a63f91f1
akumar <abhinav.kumar@navihealth.com>
2020-08-22 09:46:24 +0530

    Implemented interpreter design pattern

---
+++ src/main/java/com/abhi/behavioral/interpreter/Expression.java
@@ -0,0 +1,5 @@
+package com.abhi.behavioral.interpreter;
+
+public interface Expression {
+    String interpret(InterpreterContext ic);
+}
---
+++ src/main/java/com/abhi/behavioral/interpreter/IntToBinaryExpression.java
@@ -0,0 +1,14 @@
+package com.abhi.behavioral.interpreter;
+
+public class IntToBinaryExpression implements Expression {
+    private int i;
+
+    public IntToBinaryExpression(int i) {
+        this.i = i;
+    }
+
+    @Override
+    public String interpret(InterpreterContext ic) {
+        return ic.getBinaryFormat(this.i);
+    }
+}
