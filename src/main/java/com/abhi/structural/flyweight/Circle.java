7e1222cfb42d01073a1b64b030b12a724382bbae
akumar <abhinav.kumar@navihealth.com>
2020-08-23 09:29:24 +0530

    Implemented flyweight design pattern

---
+++ src/main/java/com/abhi/structural/flyweight/Circle.java
@@ -0,0 +1,49 @@
+package com.abhi.structural.flyweight;
+
+public class Circle implements Shape {
+    private String color;
+    private int x;
+    private int y;
+    private int radius;
+
+    public Circle(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing circle of color: " + color + " at x: " + x + " and y: " + y);
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+}
---
+++ src/main/java/com/abhi/structural/flyweight/Shape.java
@@ -0,0 +1,5 @@
+package com.abhi.structural.flyweight;
+
+public interface Shape {
+    void draw();
+}
