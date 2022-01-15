0900550bced78b85b5b0968f7316cb443b1a9d46
akumar <abhinavoist@gmail.com>
2022-01-15 03:58:22 -0500

    Added builder design pattern improvement

--- src/main/java/com/abhi/creational/builder/Main.java
+++ src/main/java/com/abhi/creational/builder/Main.java
@@ -1,5 +1,4 @@
 package com.abhi.creational.builder;
-
 public class Main {
     public static void main(String[] args) {
         User user1 = new User.UserBuilder("Abhinav", "Kumar")
@@ -13,5 +12,15 @@ public class Main {
 
         System.out.println(user1);
         System.out.println(user2);
+
+        NewUser newUser1 = NewUser.NewUserBuilder.newUser()
+                .setFirstName("Abhi")
+                .setLastName("Singh")
+                .setAge(33)
+                .build();
+        NewUser newUser2 = NewUser.NewUserBuilder.newUser("Nita", "Singh")
+                .build();
+        System.out.println(newUser1);
+        System.out.println(newUser2);
     }
 }
